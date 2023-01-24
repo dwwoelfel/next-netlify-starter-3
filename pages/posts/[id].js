ff889de7aea97cefad960e51457fa0df3b9cf405
Daniel Woelfel <dwwoelfel@gmail.com>
2023-01-24 08:59:46 -0800

    one minute revalidation

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -8,10 +8,7 @@ export async function getStaticProps(props) {
       dateS: new Date(now).toLocaleString(),
       id: parseInt(props.params.id, 10),
     },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 10, // In seconds
+    revalidate: 60,
   };
 }
 
