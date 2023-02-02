4f88580b6129f177c3bcfb2f3ddcac09c5f3b02e
Daniel Woelfel <dwwoelfel@gmail.com>
2023-02-02 08:34:30 -0800

    test refresh endpoint

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -3,17 +3,28 @@ const handler = async (event, context) => {
   console.log('event', event);
   console.log('context', context);
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const res = await fetch(
+      'https://api.netlify.com/api/v1/sites/40ce9daa-744e-471e-ba5c-afa7cbac4c42/refresh_on_demand_builders',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${context.clientContext.custom.odb_refresh_hooks}`,
+        },
+      },
+    );
+    const json = await res.json();
+    console.log('res', json);
+    const subject = event.queryStringParameters.name || 'World';
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({message: `Hello ${subject}`}),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
-    }
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return {statusCode: 500, body: error.toString()};
   }
-}
+};
 
-module.exports = { handler }
+module.exports = {handler};
