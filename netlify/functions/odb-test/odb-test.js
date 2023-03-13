bb741ae41b19bbbe3adda568464853b04cde3c51
Daniel Woelfel <dwwoelfel@gmail.com>
2023-03-13 15:42:59 -0700

    gzip

--- netlify/functions/odb-test/odb-test.js
+++ netlify/functions/odb-test/odb-test.js
@@ -1,14 +1,19 @@
-const { builder } = require("@netlify/functions")
-const twitterJson = require('./twitterJson.json')
+const {builder} = require('@netlify/functions');
+const twitterJson = require('./twitterJson.json');
+const {gzip} = require('node-gzip');
 
 const originalResponse = {
   body: JSON.stringify(twitterJson, null, 2),
   statusCode: 200,
   ttl: 60,
-}
+};
 
 async function handler(event, context) {
-  return originalResponse
+  if (event.path.endsWith('gzip')) {
+    const compressed = await gzip(originalResponse.body);
+    return {...originalResponse, body: compressed};
+  }
+  return originalResponse;
 }
 
 exports.handler = builder(handler);
